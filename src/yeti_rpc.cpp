11e8a5f070e8de88850716aadaf91176ddbbd502
Michael Furmur <michael.f@didww.com>
2016-07-27 09:35:38 +0300

    rpc: fix HttpUploadEven constructor call

--- src/yeti_rpc.cpp
+++ src/yeti_rpc.cpp
@@ -910,7 +910,10 @@ void YetiRpc:: requestUpload(const AmArg& args, AmArg& ret) {
     if (AmSessionContainer::instance()->postEvent(
         HTTP_EVENT_QUEUE,
         //new HttpUploadEvent(destination_id,args.get(1).asCStr())))
-        new HttpUploadEvent(args.get(0).asCStr(),args.get(1).asCStr(),args.get(2).asCStr())))
+        new HttpUploadEvent(args.get(0).asCStr(),
+                            args.get(1).asCStr(),
+                            args.get(2).asCStr(),
+                            string())))
     {
         ret = "posted to queue";
     } else {
