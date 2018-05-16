eab621ba6a6dc43319efc82cc06ed2a111d02603
Michael Furmur <m.furmur@gmail.com>
2018-05-16 15:15:41 +0300

    update yeti.conf.cmake

--- etc/yeti.conf.cmake
+++ etc/yeti.conf.cmake
@@ -1,23 +1,17 @@
-### common config ###
-
-# node_id
-#	yeti instance id
-#	default: mandatory
-#node_id = 0
-
 # cfg_timeout
 #	maximum time for waiting remote config (ms)
 #	default: 5000
 #cfg_timeout=5000
 
-# cfg_urls
-#	comma separated list of remote config servers
-#	default: mandatory
-cfg_urls = main
+# cfg_host
+#	SCTP endpoint host
+#	default: 127.0.0.1
+#cfg_host=127.0.0.1
 
-# cfg_urls_<cfg_url_name>
-#	comma separated list of remote config servers
-#	default: mandatory
-cfg_url_main = tcp://127.0.0.1:4444
+# cfg_port
+#	SCTP endpoint host
+#	default: 4444
+#cfg_port=4444
 
+# whether to sent 200OK for OPTIONS requests
 core_options_handling=yes
