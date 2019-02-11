85fcc4066124580ea288cf612092bbc23206b4ff
Michael Furmur <m.furmur@gmail.com>
2019-02-11 17:03:05 +0200

    update dist cfg

--- etc/yeti.conf.cmake
+++ etc/yeti.conf.cmake
@@ -1,17 +1,18 @@
-# cfg_timeout
-#	maximum time for waiting remote config (ms)
-#	default: 5000
-#cfg_timeout=5000
+module "yeti" {
+    mgmt {
+        # config server host
+        #host = localhost
 
-# cfg_host
-#	SCTP endpoint host
-#	default: 127.0.0.1
-#cfg_host=127.0.0.1
+        # config server port
+        #port = 4444
 
-# cfg_port
-#	SCTP endpoint host
-#	default: 4444
-#cfg_port=4444
+        # maximum time for waiting remote config (ms)
+        #timeout = 5000
+    }
 
-# whether to sent 200OK for OPTIONS requests
-core_options_handling=yes
+    # process OPTIONS requests
+    #core_options_handling = yes
+
+    # enable registrations client
+    #registrations_enabled = yes
+}
