yeti-switch/sems-yeti

commit 28d29bf8a5be727ad097f053cde4f10d5972f7d4

SBCCallLeg: guard SDP processing with call_ctx mutex

src/SBCCallLeg.cpp
@@ -1380,6 +1380,7 @@ int SBCCallLeg::relayEvent(AmEvent* ev)
 
         try {
             int res;
+            AmLock l(*call_ctx);
             if(req.method==SIP_METH_ACK){
                 //ACK can contain only answer
                 dump_SdpMedia(call_ctx->bleg_negotiated_media,"bleg_negotiated media_pre");
@@ -1518,6 +1519,7 @@ int SBCCallLeg::relayEvent(AmEvent* ev)
 
             try {
                 int res;
+                AmControlledLock l(*call_ctx);
                 if(dlg_oa_state==AmOfferAnswer::OA_OfferRecved){
                     DBG("relayEvent(): process offer in reply");
                     res = processSdpOffer(
@@ -1553,6 +1555,7 @@ int SBCCallLeg::relayEvent(AmEvent* ev)
                 }
 
                 if(res != 0){
+                    l.release_ownership();
                     terminateLegOnReplyException(
                         reply,
                         InternalException(res, call_ctx->getOverrideId(a_leg)));
@@ -1665,6 +1668,7 @@ void SBCCallLeg::onSipRequest(const AmSipRequest& req)
 
             AmSipRequest upd_req(req);
             try {
+                AmLock l(*call_ctx);
                 int res = processSdpOffer(
                     this, call_profile,
                     upd_req.body, upd_req.method,
@@ -1745,6 +1749,7 @@ void SBCCallLeg::onSipRequest(const AmSipRequest& req)
 
             AmSipRequest inv_req(req);
             try {
+                AmLock l(*call_ctx);
                 int res = processSdpOffer(
                     this, call_profile,
                     inv_req.body, inv_req.method,

commit 6e37492f07c3fb11b4ef3250d5fe89e33ab5d963

fix deadlock introduced by 28d29bf8a5be727ad097f053cde4f10d5972f7d4

src/SBCCallLeg.cpp
@@ -1528,43 +1528,44 @@ int SBCCallLeg::relayEvent(AmEvent* ev)
 
             try {
                 int res;
-                AmControlledLock l(*call_ctx);
-                if(dlg_oa_state==AmOfferAnswer::OA_OfferRecved){
-                    DBG("relayEvent(): process offer in reply");
-                    res = processSdpOffer(
-                        this, call_profile,
-                        reply.body, reply.cseq_method,
-                        call_ctx->get_self_negotiated_media(a_leg),
-                        a_leg ? call_profile.static_codecs_aleg_id : call_profile.static_codecs_bleg_id,
-                        false,
-                        a_leg ? call_profile.aleg_single_codec : call_profile.bleg_single_codec
-                    );
-                    if(res==0){
-                        res = filterSdpOffer(
-                            this,
-                            reply,
-                            call_profile,
-                            reply.body, reply.cseq_method,
-                            a_leg ? call_profile.static_codecs_bleg_id : call_profile.static_codecs_aleg_id
-                        );
+                { //scope for call_ctx mutex AmLock
+                    AmLock l(*call_ctx);
+                    if(dlg_oa_state==AmOfferAnswer::OA_OfferRecved){
+                        DBG("relayEvent(): process offer in reply");
+                        res = processSdpOffer(
+                                    this, call_profile,
+                                    reply.body, reply.cseq_method,
+                                    call_ctx->get_self_negotiated_media(a_leg),
+                                    a_leg ? call_profile.static_codecs_aleg_id : call_profile.static_codecs_bleg_id,
+                                    false,
+                                    a_leg ? call_profile.aleg_single_codec : call_profile.bleg_single_codec
+                                            );
+                        if(res==0){
+                            res = filterSdpOffer(
+                                        this,
+                                        reply,
+                                        call_profile,
+                                        reply.body, reply.cseq_method,
+                                        a_leg ? call_profile.static_codecs_bleg_id : call_profile.static_codecs_aleg_id
+                                                );
+                        }
+                    } else {
+                        DBG("relayEvent(): process asnwer in reply");
+                        res = processSdpAnswer(
+                                    this,
+                                    reply,
+                                    reply.body, reply.cseq_method,
+                                    call_ctx->get_other_negotiated_media(a_leg),
+                                    a_leg ? call_profile.bleg_single_codec : call_profile.aleg_single_codec,
+                                    call_profile.filter_noaudio_streams,
+                                    //final positive reply MUST contain SDP answer if we sent offer
+                                    (dlg_oa_state==AmOfferAnswer::OA_OfferSent
+                                     && reply.code >= 200 && reply.code < 300)
+                                    );
                     }
-                } else {
-                    DBG("relayEvent(): process asnwer in reply");
-                    res = processSdpAnswer(
-                        this,
-                        reply,
-                        reply.body, reply.cseq_method,
-                        call_ctx->get_other_negotiated_media(a_leg),
-                        a_leg ? call_profile.bleg_single_codec : call_profile.aleg_single_codec,
-                        call_profile.filter_noaudio_streams,
-                        //final positive reply MUST contain SDP answer if we sent offer
-                        (dlg_oa_state==AmOfferAnswer::OA_OfferSent
-                            && reply.code >= 200 && reply.code < 300)
-                    );
                 }
 
                 if(res != 0){
-                    l.release_ownership();
                     terminateLegOnReplyException(
                         reply,
                         InternalException(res, call_ctx->getOverrideId(a_leg)));