24a444372e8d6a9689dded3f747b412bbfd87a3f
Michael Furmur <furmur@pm.me>
2023-02-07 17:10:30 +0200

    relay DTMF volume level

--- src/SBCCallLeg.cpp
+++ src/SBCCallLeg.cpp
@@ -2421,9 +2421,10 @@ void SBCCallLeg::onOtherBye(const AmSipRequest& req)
 
 void SBCCallLeg::onDtmf(AmDtmfEvent* e)
 {
-    DBG("received DTMF on %cleg (%i;%i) source:%d",
+    DBG("received DTMF on %cleg (%i;%i;%i) source:%d",
         a_leg ? 'A': 'B',
-        e->event(), e->duration(), e->event_id);
+        e->event(), e->duration(), e->volume(),
+        e->event_id);
 
     AmControlledLock call_ctx_lock(*call_ctx_mutex);
     getCtx_void;
@@ -2476,8 +2477,11 @@ void SBCCallLeg::onDtmf(AmDtmfEvent* e)
         if(ms) {
             DBG("sending DTMF (%i;%i)", e->event(), e->duration());
             call_ctx_lock.release();
-            ms->sendDtmf(!a_leg,
-                e->event(),static_cast<unsigned int>(e->duration()));
+            ms->sendDtmf(
+                !a_leg,
+                e->event(),
+                static_cast<unsigned int>(e->duration()),
+                e->volume());
         }
     } break;
     case DTMF_TX_MODE_INFO_DTMF_RELAY:
